634de2fa63e61014c88d0df257d9077dd548487d
zedr0n <zedr0nre@gmail.com>
2019-04-21 10:08:03 +0100

    add aspnetcore graphql

---
+++ Chronos.GraphQL.AspNetCore/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Chronos.Coins;
+using HotChocolate;
+using HotChocolate.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleInjector;
+using Schema = Chronos.Coins.Schema;
+
+namespace Chronos.GraphQL.AspNetCore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var container = new Container();
+            var schema = ZES.GraphQL.Startup.WireGraphQl(container, Config.RegisterAll,
+                typeof(Schema.Query), typeof(Schema.Mutation));
+            services.AddGraphQL(schema);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseGraphQL();
+        }
+    }
+}
